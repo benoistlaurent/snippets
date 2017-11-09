19ad65f1bec91cf2a01e27a329f0097a5d66fdaf
Benoist LAURENT <benoist.laurent@gmail.com>
2017-11-09 13:54:33 +0100

    make_dum_data.py: write more 0.0s

--- csv_to_sql/make_dum_data.py
+++ csv_to_sql/make_dum_data.py
@@ -3,13 +3,16 @@
 
 import random
 
+def choose_random():
+    p = random.random() 
+    if p < 0.7:
+        return 0.0
+    return p
 
-def create_csv_file(path, file_format='big'):
+
+def create_csv_file(path, nrows, ncols, file_format='big'):
     """Create a single csv file with 100k rows and 720 columns."""
-    nrows = 100000
-    ncols = 720
-
-    float_fmt = '{:.4e}'
+    float_fmt = '{:.4g}'
     if file_format == 'big':
         float_fmt = '{:.18e}'
     elif file_format != 'small':
@@ -19,15 +22,14 @@ def create_csv_file(path, file_format='big'):
     with open(path, 'wt') as f:
         for i in range(nrows):
             rowid = '{:07d}'.format(i + 1)
-            cols = ['{:.18e}'.format(random.random()) for i in range(ncols)]
-            cols = ['{:.4e}'.format(random.random()) for i in range(ncols)]
+            cols = [float_fmt.format(choose_random()) for i in range(ncols)]
             print("{},{}".format(rowid, ','.join(cols)), file=f)
             print("\r{}: progress: {:.0%}".format(path, (i + 1) / nrows), end='', flush=True)
         print()
 
 
 def main():
-    create_csv_file('output_small.csv', file_format='small')
+    create_csv_file('output_small.csv', nrows=400000, ncols=720, file_format='small')
 
     # for i in range(6):
     #     path = 'output_{}.csv'.format(i + 1)
